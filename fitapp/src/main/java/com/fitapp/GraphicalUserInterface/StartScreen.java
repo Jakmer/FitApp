8d03ee75767b8fe4c98d669f746f19cb847177a9
Jakmer <kubamerta13@gmail.com>
2024-01-16 00:01:21 +0100

    Add mainpage. Login is ready to extend

--- fitapp/src/main/java/com/fitapp/GraphicalUserInterface/StartScreen.java
+++ fitapp/src/main/java/com/fitapp/GraphicalUserInterface/StartScreen.java
@@ -5,15 +5,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class StartScreen extends Application {
 
+    public Label incorrectLabel;
     @FXML
     private javafx.scene.control.TextField usernameField;
     @FXML
     private javafx.scene.control.TextField passwordField;
+
     @Override
     public void start(Stage primaryStage) {
 
@@ -27,9 +30,11 @@ public class StartScreen extends Application {
 
     /**
      * This method creates the login screen
+     *
      * @param primaryStage
      */
     private void loginScreen(Stage primaryStage) throws IOException {
+        primaryStage.setResizable(false);
         Parent root = FXMLLoader.load(getClass().getResource("/LoginScreen.fxml"));
 
         Scene scene = new Scene(root, 817, 812);
@@ -45,8 +50,39 @@ public class StartScreen extends Application {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
+        /**
+         * database is responsible for checking if the user exists and if the password is correct
+         * for let's suppose that the username is "admin" and the password is "admin"
+         */
         System.out.println(username);
         System.out.println(password);
-    }
 
+        if(username.isEmpty() || password.isEmpty()){
+            incorrectLabel.setText("Please enter username and password");
+            incorrectLabel.setVisible(true);
+            usernameField.clear();
+            passwordField.clear();
+        }
+        else if (username.equals("admin") && password.equals("admin")) {
+            System.out.println("Login successful");
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/MainScreen.fxml"));
+            Scene scene = new Scene(root, 817, 812);
+            Stage primaryStage = new Stage();
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("FitApp");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } else {
+            incorrectLabel.setVisible(true);
+            usernameField.clear();
+            passwordField.clear();
+        }
+    }
 }
+
+
+
+
+
